71123ce83c7637b2df85811b2bcae3439fb5b05c
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-06-24 14:20:50 +0530

    Difference b/w React Functional Component and React Element

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Core React Syntax
-const heading = React.createElement(
-  "h1",
-  { id: "heading", class: "head" },
-  "Hello World from javascript!"
-);
+//Two types of component:
+//1. React Class Component
+//2. React Functional Component
 
-//JSX syntax (React Element)
-const jsxHeading = (
+//JSX syntax (React Element) - Naming Convention, always starts with small letter
+const heading = (
   <h1 id="heading" className="head">
     Namaste React from JSX!
   </h1>
 );
 
+//React Functional Component - Naming Convention, always starts with Capital letter
+const HeadingComponent = () => (
+  <h1>Namaste React from React Functional Component</h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+//root.render(heading) --> This is how a element is rendered
+root.render(<HeadingComponent />); //This is how a component is rendered
