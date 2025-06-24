919e6cae282e0fe600955050979775ab626e79e3
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-06-24 14:37:41 +0530

    arrow function v/s normal function in a component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Component Composition - is putting component inside another component.
+//old way of writing using normal function syntax but requires a return keyword
+const TitleComponent = function () {
+  return <h1>Namaste React from Title Component🚀</h1>;
+};
 
-const TitleComponent = () => <h1>Namaste React from Title Component🚀</h1>;
-
+//arrow function mostly seen in practice
 const HeadingComponent = () => (
   <div id="container">
     <TitleComponent />
@@ -13,6 +15,4 @@ const HeadingComponent = () => (
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//root.render(heading) --> This is how a element is rendered
 root.render(<HeadingComponent />); //This is how a component is rendered
