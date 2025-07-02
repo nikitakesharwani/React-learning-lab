333b14ed5bd13dc539753ce6d9e2a6a2f2ba2c70
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-07-02 10:45:37 +0530

    create restaurant card

--- App.js
+++ App.js
@@ -1,18 +1,78 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
-//old way of writing using normal function syntax but requires a return keyword
-const TitleComponent = function () {
-  return <h1>Namaste React from Title Component🚀</h1>;
+const styleCard = {
+  backgroundColor: "#f0f0f0",
 };
 
-//arrow function mostly seen in practice
-const HeadingComponent = () => (
-  <div id="container">
-    <TitleComponent />
-    <h1>Namaste React from React Functional Component</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/1/21/f79966f8-7e56-4233-8446-4f19d1bb7ac5_1030945.JPG"
+      />
+      <h3>Meghna Foods</h3>
+      <h4>Biryani, North Indian, Asian</h4>
+      <h4>4.4 star</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />); //This is how a component is rendered
+root.render(<AppLayout />);
