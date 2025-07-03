5120baffe7f507e061f9fab7990d5d36274dd283
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-07-03 18:05:28 +0530

    onClick event added to top rated restaurant to filter restaurant but the front end is not updating, so moving forward will introduce useState hook.

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,11 +2,68 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  let ListOfRestaurant = [
+    {
+      info: {
+        id: "5938",
+        name: "Burger King",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
+        costForTwo: "₹350 for two",
+        cuisines: ["Burgers", "American"],
+        avgRating: 3.4,
+        sla: {
+          deliveryTime: 33,
+        },
+      },
+    },
+    {
+      info: {
+        id: "5935",
+        name: "KFC",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
+        costForTwo: "₹350 for two",
+        cuisines: ["Burgers", "American"],
+        avgRating: 4.1,
+        sla: {
+          deliveryTime: 33,
+        },
+      },
+    },
+    {
+      info: {
+        id: "5936",
+        name: "Mac Donald",
+        cloudinaryImageId:
+          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
+        costForTwo: "₹350 for two",
+        cuisines: ["Burgers", "American"],
+        avgRating: 4.4,
+        sla: {
+          deliveryTime: 33,
+        },
+      },
+    },
+  ];
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            ListOfRestaurant = ListOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            console.log(ListOfRestaurant);
+          }}
+        >
+          Top rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {ListOfRestaurant.map((restaurant) => (
           <RestaurantCard resData={restaurant} key={restaurant.info.id} /> //this is data driven UI
         ))}
       </div>
