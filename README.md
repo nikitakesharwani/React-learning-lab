6cdad4ac6d6d546f5a3e5cf60b37f6e15324da49
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-07-04 16:10:51 +0530

    resList passed to useState to update UI using react hook

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 React Project from Beginning to Pro
 
-Two type of export/import
+# Two type of export/import
 
 -Default Export/Import
 export default Component
@@ -13,3 +13,21 @@ export const Component
 import {Component} from "path"
 
 We can export a component as named as well as default export
+
+# React hooks
+
+1. Normal JS utility functions written inside react library. These hooks are imported as named import.
+2. Whenever in react, the state variable changes/updates from anywhere. React will re-renders the component.
+3. Two important hooks-
+
+- useState() - 80% usage Superpowerful react variables
+- useEffect() - 20% usage
+
+4. React is good at DOM manipulation using useState hook.
+
+# Important Terminologies
+
+Reconciliation Algorithm(react fiber) introduced in react 16
+Virtual DOM - representation of actual DOM, React objects/react element is virtual DOM
+Actual DOM - the elements we see in developer tool
+Diff Algorithm - finds the difference between 2 virtual DOM, updted and previous virtual DOM
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,51 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
-  let ListOfRestaurant = [
-    {
-      info: {
-        id: "5938",
-        name: "Burger King",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Burgers", "American"],
-        avgRating: 3.4,
-        sla: {
-          deliveryTime: 33,
-        },
-      },
-    },
-    {
-      info: {
-        id: "5935",
-        name: "KFC",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Burgers", "American"],
-        avgRating: 4.1,
-        sla: {
-          deliveryTime: 33,
-        },
-      },
-    },
-    {
-      info: {
-        id: "5936",
-        name: "Mac Donald",
-        cloudinaryImageId:
-          "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/b88fe4d2-557a-42a5-a801-a43a40134004_5938.jpg",
-        costForTwo: "₹350 for two",
-        cuisines: ["Burgers", "American"],
-        avgRating: 4.4,
-        sla: {
-          deliveryTime: 33,
-        },
-      },
-    },
-  ];
+
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
 
   return (
     <div className="body">
@@ -53,17 +12,17 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            ListOfRestaurant = ListOfRestaurant.filter(
+            const filteredList = listOfRestaurant.filter(
               (res) => res.info.avgRating > 4
             );
-            console.log(ListOfRestaurant);
+            setListOfRestaurant(filteredList);
           }}
         >
           Top rated Restaurant
         </button>
       </div>
       <div className="res-container">
-        {ListOfRestaurant.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestaurantCard resData={restaurant} key={restaurant.info.id} /> //this is data driven UI
         ))}
       </div>
