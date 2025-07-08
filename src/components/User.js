d42bb66e7c293065f4247513a72e6bf89f95bf54
nikitakesharwani <nikitakesharwani1708@gmail.com>
2025-07-08 20:58:02 +0530

    update the state variable from class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const User = ({ name, location }) => {
+  const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
+      <h1>Count - {count}</h1>
+      <h1>Count - {count2}</h1>
       <h3>User Name : {name} </h3>
       <h3>Location : {location} </h3>
       <h3>Contact : 9254897546 </h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,12 +2,29 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>Count - {count}</h1>
+        <button
+          onClick={() => {
+            //NEVER UPDATE STATE VARIABLES DIRECTLY
+            this.setState({
+              count: this.state.count + 1,
+              count2: this.state.count2 + 1,
+            });
+          }}
+        >
+          Increase Count
+        </button>
+        <h1>Count - {count2}</h1>
         <h3>User Name : {name} </h3>
         <h3>Location : {location} </h3>
         <h3>Contact : 9254897546 </h3>
